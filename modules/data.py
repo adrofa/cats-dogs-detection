19ed151e4974f75a109c422e5860946a8d093e07
adrofa <artem.drofa@gmail.com>
2021-05-14 18:28:44 +0300

    huge update

--- modules/data.py
+++ modules/data.py
@@ -11,6 +11,9 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 
 import cv2
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+from torch.utils.data import Dataset
 
 
 def pkl_dump(obj, file):
@@ -26,67 +29,100 @@ def pkl_load(file):
     return obj
 
 
-def get_dataset_df(dataset_dir, verbose=False):
-    """Collect pandas.DataFrame with *.jpg/*.txt files' names as index
-    (<file_name.jpg> / <file_name.txt>) and the following columns:
-        img_path - path to the *.jpg file;
-        cls - image class (1 - cat; 2 - dog);
-        xmin, ymin, xmax, ymax - bounding box' coordinates;
-        height, width - image's height and width.
+class MyDataset(Dataset):
+    def __init__(self, dataset_dir, transform=None, verbose=False):
+        self.dataset_dir = dataset_dir
+        self.df = self.get_dataset_df(dataset_dir, verbose)
+        self.transform = A.Compose(
+            [transform, ToTensorV2(p=1)],
+            bbox_params=A.BboxParams(format='albumentations', label_fields=['class_labels'])
+        )
 
-    Args:
-        dataset_dir (str): path to directory with images and annotation
-        verbose (str): if `str` - description of the progress bar;
-            if `False` - no progress bar.
+    def __len__(self):
+        return len(self.df)
 
-    Returns:
-         pandas.DataFrame
-    """
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        img = cv2.cvtColor(cv2.imread(str(row["img_path"])), cv2.COLOR_BGR2RGB)
+        transformed = self.transform(image=img, bboxes=[row["bboxes"]], class_labels=[row["cls"]])
+        return transformed["image"], transformed["bboxes"], transformed["class_labels"]
 
-    cols = ["id", "img_path", "height", "width",
-            "cls", "xmin", "ymin", "xmax", "ymax"]
-    dataset_df = pd.DataFrame(columns=cols)
+    def get_img_bboxes(self, idx, transformed=True):
+        if transformed:
+            img, bboxes, _ = self.__getitem__(idx)
+            img, bboxes = img.numpy().transpose(1, 2, 0), bboxes[0]
+            bboxes = [int(bboxes[i] * img.shape[(i + 1) % 2])
+                      for i, _ in enumerate(bboxes)]
+        else:
+            row = self.df.iloc[idx]
+            img = cv2.cvtColor(cv2.imread(str(row["img_path"])), cv2.COLOR_BGR2RGB)
+            bboxes = [row["xmin"], row["ymin"], row["xmax"], row["ymax"]]
 
-    txt_files = [f for f in os.listdir(dataset_dir) if ".txt" in f]
-    with tqdm(txt_files, desc=verbose, file=sys.stdout, disable=not verbose) as iterator:
-        for file in iterator:
-            img_id = file.split(".")[0]
+        return img, bboxes
 
-            img_path = dataset_dir / (img_id + ".jpg")
-            height, width = cv2.imread(str(img_path)).shape[:2]
+    @staticmethod
+    def get_dataset_df(dataset_dir, verbose=False):
+        """Collect pandas.DataFrame with *.jpg/*.txt files' names as index
+        (<file_name.jpg> / <file_name.txt>) and the following columns:
+            img_path - path to the *.jpg file;
+            cls - image class (1 - cat; 2 - dog);
+            xmin, ymin, xmax, ymax - bounding box' coordinates;
+            xmin_n, ymin_n, xmax_n, ymax_n - normalized bounding box' coordinates;
+            height, width - image's height and width.
 
-            with open(dataset_dir / file, "r") as f:
-                annotation = map(int, f.read().split())
-            img_dct = {k: v for k, v in zip(cols[-5:], annotation)}
+        Args:
+            dataset_dir (str): path to directory with images and annotation
+            verbose (str): if `str` - description of the progress bar;
+                if `False` - no progress bar.
 
-            img_dct.update({
-                "id": img_id,
-                "img_path": img_path,
-                "height": height,
-                "width": width,
-            })
+        Returns:
+            dataset_df: pandas.DataFrame.
+        """
 
-            dataset_df = dataset_df.append(img_dct, ignore_index=True)
+        inf_cols = ["id", "img_path", "height", "width"]
+        ann_cols = ["cls", "xmin", "ymin", "xmax", "ymax"]
+        dataset_df = pd.DataFrame(columns=inf_cols + ann_cols)
 
-    for col in cols[-7:]:
-        dataset_df[col] = dataset_df[col].astype(int)
-    dataset_df.set_index("id", drop=True, inplace=True)
+        txt_files = [f for f in os.listdir(dataset_dir) if ".txt" in f]
+        with tqdm(txt_files, desc=verbose, file=sys.stdout, disable=not verbose) as iterator:
+            for file in iterator:
+                img_id = file.split(".")[0]
 
-    return dataset_df
+                # get image height and width
+                img_path = dataset_dir / (img_id + ".jpg")
+                height, width = cv2.imread(str(img_path)).shape[:2]
+
+                # read annotation: class + bboxes
+                with open(dataset_dir / file, "r") as f:
+                    annotation = map(int, f.read().split())
+
+                # append row to df
+                img_dct = {k: v for k, v in zip(ann_cols, annotation)}
+                img_dct.update({
+                    "id": img_id,
+                    "img_path": img_path,
+                    "height": height,
+                    "width": width,
+                })
+                dataset_df = dataset_df.append(img_dct, ignore_index=True)
+
+        # converting annotation columns type to int
+        for col in ann_cols:
+            dataset_df[col] = dataset_df[col].astype(int)
+
+        # bbox normalization
+        dataset_df["bboxes"] = (dataset_df[ann_cols[1:]].values /
+                                dataset_df[["width", "height"] * 2].values).tolist()
+
+        return dataset_df.set_index("id", drop=True)
 
 
-def dataset_row_preview(row):
+def preview(img, bboxes):
     """Show an image with a bounding box from a dataset row."""
-
-    # read
-    img = cv2.cvtColor(cv2.imread(str(row["img_path"])), cv2.COLOR_BGR2RGB)
-    # add bounding box
-    img = cv2.rectangle(
+    plt.imshow(cv2.rectangle(
         img=img,
-        pt1=(row["xmin"], row["ymin"]),
-        pt2=(row["xmax"], row["ymax"]),
+        pt1=(bboxes[0], bboxes[1]),
+        pt2=(bboxes[2], bboxes[3]),
         color=(0, 255, 0),
         thickness=2
-    )
-
-    plt.imshow(img)
+    ))
